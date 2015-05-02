04ff97e4e36813b014b9eac6178fcf77b4965bb2
AenimaAut <albert.kraschl@gmx.at>
2015-05-02 12:13:46 +0200

    added some Classes

---
+++ app/src/main/java/com/gamble/unopp/connection/Request.java
@@ -0,0 +1,7 @@
+package com.gamble.unopp.connection;
+
+/**
+ * Created by Albert on 02.05.2015.
+ */
+public class Request {
+}
---
+++ app/src/main/java/com/gamble/unopp/model/cards/ActionCard.java
@@ -0,0 +1,13 @@
+package com.gamble.unopp.model.cards;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Albert on 02.05.2015.
+ */
+public class ActionCard extends Card {
+
+    public ActionCard(int ID, Bitmap image) {
+        super(ID, image);
+    }
+}
