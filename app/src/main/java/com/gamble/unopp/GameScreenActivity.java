e25363fa2f926e38ae989c9076d87a6c8cc6d5ab
Mario Graf <mario.graf@bitmovin.net>
2015-05-05 19:36:04 +0200

    implemented dragging of cards from hand onto played stack.

--- app/src/main/java/com/gamble/unopp/GameScreenActivity.java
+++ app/src/main/java/com/gamble/unopp/GameScreenActivity.java
@@ -1,12 +1,23 @@
 package com.gamble.unopp;
 
+import android.content.ClipData;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.MimeTypeMap;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.gamble.unopp.listener.CardLongClickListener;
+import com.gamble.unopp.listener.PlayedStackOnDragListener;
 import com.gamble.unopp.model.cards.Card;
 import com.gamble.unopp.model.game.CardDeck;
 
@@ -14,24 +25,37 @@ import com.gamble.unopp.model.game.CardDeck;
 public class GameScreenActivity extends ActionBarActivity {
 
     private LinearLayout llHand;
+    private FrameLayout flPlayedCards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
+
+        // remove title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_game_screen);
 
+        // get views
+        this.llHand             = (LinearLayout) findViewById(R.id.llHand);
+        this.flPlayedCards      = (FrameLayout) findViewById(R.id.flPlayedCards);
 
         CardDeck deck = new CardDeck();
 
-        for (Card card : deck.getDeck()) {
+        for (final Card card : deck.getDeck()) {
 
-            this.llHand = (LinearLayout) findViewById(R.id.llHand);
-
-            ImageView   imageView = new ImageView(getBaseContext());
+            final ImageView   imageView = new ImageView(getBaseContext());
             imageView.setImageBitmap(card.getImage());
+            imageView.setTag(card);
+            imageView.setOnLongClickListener(new CardLongClickListener(imageView));
 
             this.llHand.addView(imageView);
         }
+
+        flPlayedCards.setOnDragListener(new PlayedStackOnDragListener(getResources().getColor(R.color.drag_over)));
     }
 
     @Override
