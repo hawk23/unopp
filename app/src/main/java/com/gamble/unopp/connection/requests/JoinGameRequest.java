f34677f3fa89191979c530f629bad4046f0c93d2
Mario Graf <mario.graf@bitmovin.net>
2015-05-14 19:47:14 +0200

    corrected parameter names.

--- app/src/main/java/com/gamble/unopp/connection/requests/JoinGameRequest.java
+++ app/src/main/java/com/gamble/unopp/connection/requests/JoinGameRequest.java
@@ -24,11 +24,11 @@ public class JoinGameRequest extends Request {
 
     public void setGameId(int gameId) {
         this.gameId = gameId;
-        this.requestParameters.put("gameId", String.valueOf(gameId));
+        this.requestParameters.put("gameID", String.valueOf(gameId));
     }
 
     public void setPlayerId(int playerId) {
         this.playerId = playerId;
-        this.requestParameters.put("playerId", String.valueOf(playerId));
+        this.requestParameters.put("playerID", String.valueOf(playerId));
     }
 }
