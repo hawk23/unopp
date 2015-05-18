03f5d09b673be5c02ee2f39dee590bdea67574d2
AenimaAut <albert.kraschl@gmx.at>
2015-05-18 12:50:01 +0200

    added GameUpdate Class - now all turns are GameUpdates

---
+++ app/src/main/java/com/gamble/unopp/model/game/GameUpdate.java
@@ -0,0 +1,13 @@
+package com.gamble.unopp.model.game;
+
+/**
+ * Created by Albert on 18.05.2015.
+ */
+abstract class GameUpdate {
+
+    protected int ID;
+
+    public GameUpdate(int ID) {
+        this.ID = ID;
+    }
+}
