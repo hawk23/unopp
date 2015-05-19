60742c9293b353bdd871197cc8557aab399d9266
rwiedenh <wiro9500@A1.net>
2015-05-19 10:15:20 +0200

    configured project for junit-testing

---
+++ app/src/test/java/com/gamble/unopp/GameLogicTest.java
@@ -0,0 +1,31 @@
+package com.gamble.unopp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by Roland on 19.05.2015.
+ */
+public class GameLogicTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void test() {
+        int result = 1;
+        int expected = 1;
+
+        assertEquals(result, expected);
+    }
+}
