46f9e7a5193e0f23cabab4261bae83e983f8a9af
Mario Graf <mario.graf@bitmovin.net>
2015-05-19 11:46:39 +0200

    update color in view after choosing color.

--- app/src/main/java/com/gamble/unopp/fragments/ChooseColorDialogFragment.java
+++ app/src/main/java/com/gamble/unopp/fragments/ChooseColorDialogFragment.java
@@ -11,6 +11,7 @@ import com.gamble.unopp.R;
 import com.gamble.unopp.model.cards.UnoColor;
 import com.gamble.unopp.model.game.Player;
 import com.gamble.unopp.model.game.Turn;
+import com.gamble.unopp.model.management.IChooseColorDialogListener;
 import com.gamble.unopp.model.management.UnoDatabase;
 
 import java.util.logging.Logger;
@@ -18,31 +19,50 @@ import java.util.logging.Logger;
 /**
  * Created by Mario on 05.05.2015.
  */
-public class ChooseColorDialogFragment extends DialogFragment {
+public class ChooseColorDialogFragment extends DialogFragment implements DialogInterface.OnClickListener {
 
     private String[] colors = new String[]{"Rot", "Grün", "Blau", "Gelb"};
+    private int selectedIndex;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.pick_color)
-                .setItems(colors, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // The 'which' argument contains the index position
-                        // of the selected item
-
-                        if (which >= 0 && which < colors.length) {
-                            /*
-                            Turn turn = new Turn(Turn.TurnType.CHOOSE_COLOR);
-                            turn.setColor(UnoColor.valueOf(colors[which]));
-                            UnoDatabase.getInstance().getCurrentGameSession().getActualGameRound().doTurn(turn);
-                            */
-                        } else {
-                            System.out.println("Unopp: ChooseColorDialogFragment has invalid index: " + which);
-                        }
-                    }
-                });
+        builder.setTitle(R.string.pick_color).setItems(colors, this);
         return builder.create();
+    }
+
+    public UnoColor getColor () {
+
+        switch (selectedIndex) {
+            case 0:
+                return UnoColor.RED;
+            case 1:
+                return UnoColor.GREEN;
+            case 2:
+                return UnoColor.BLUE;
+            case 3:
+                return UnoColor.YELLOW;
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int which) {
+        dialogInterface.cancel();
+
+        if (which >= 0 && which < colors.length) {
+
+            selectedIndex = which;
+
+            if (getActivity() != null && getActivity() instanceof IChooseColorDialogListener) {
+
+                IChooseColorDialogListener listener = (IChooseColorDialogListener) getActivity();
+                listener.onDialogClosed(this);
+            }
+        } else {
+            System.out.println("Unopp: ChooseColorDialogFragment has invalid index: " + which);
+        }
     }
 }
