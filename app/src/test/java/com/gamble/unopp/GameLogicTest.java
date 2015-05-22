5e5747abaf6cff33eaf0d5d5f44e2b3a773ba35f
rwiedenh <wiro9500@A1.net>
2015-05-22 13:01:07 +0200

    added some test cases for methods doTurn and checkCard (GameLogic)

--- app/src/test/java/com/gamble/unopp/GameLogicTest.java
+++ app/src/test/java/com/gamble/unopp/GameLogicTest.java
@@ -1,6 +1,9 @@
 package com.gamble.unopp;
 
 import com.gamble.unopp.logic.GameLogic;
+import com.gamble.unopp.model.cards.Action;
+import com.gamble.unopp.model.cards.ActionCard;
+import com.gamble.unopp.model.cards.ActionType;
 import com.gamble.unopp.model.cards.Card;
 import com.gamble.unopp.model.cards.NumberCard;
 import com.gamble.unopp.model.cards.UnoColor;
@@ -29,6 +32,11 @@ public class GameLogicTest {
     private Player player;
     private NumberCard card;
     private Turn turn;
+    private ArrayList<Card> playedStack;
+    private ArrayList<Card> stack;
+    private ArrayList<Card> hand;
+    private ActionType actionType1;
+    private ActionType actionType2;
 
     @Before
     public void setUp() {
@@ -36,6 +44,12 @@ public class GameLogicTest {
         gameRound = new GameRound(1, gameSession);
         gameState = new GameState(gameRound);
         gameLogic = new GameLogic(gameState);
+        player = new Player(1, "Test");
+        playedStack = new ArrayList();
+        stack = new ArrayList();
+        hand = new ArrayList();
+        actionType1 = new ActionType(0);
+        actionType2 = new ActionType(0);
     }
 
     @After
@@ -47,13 +61,16 @@ public class GameLogicTest {
         player = null;
         card = null;
         turn = null;
+        playedStack = null;
+        stack = null;
+        hand = null;
+        actionType1 = null;
+        actionType2 = null;
     }
 
     @Test
     public void testCheckTurnDraw() {
-        player = new Player(1, "Test");
         player.setHasToChooseColor(false);
-
         turn = new Turn(1, Turn.TurnType.DRAW, player);
 
         assertTrue(gameLogic.checkTurn(turn));
@@ -61,12 +78,9 @@ public class GameLogicTest {
 
     @Test
     public void testCheckTurnPlayCorrectCard() {
-        ArrayList<Card> playedStack = new ArrayList();
         playedStack.add(new NumberCard(1, 2, null, UnoColor.BLUE));
-
         gameState.setPlayedStack(playedStack);
 
-        player = new Player(1, "Test");
         card = new NumberCard(1, 2, null, UnoColor.RED);
 
         turn = new Turn(1, Turn.TurnType.PLAY_CARD, player);
@@ -77,12 +91,9 @@ public class GameLogicTest {
 
     @Test
     public void testCheckTurnPlayWrongCard() {
-        ArrayList<Card> playedStack = new ArrayList();
         playedStack.add(new NumberCard(1, 2, null, UnoColor.BLUE));
-
         gameState.setPlayedStack(playedStack);
 
-        player = new Player(1, "Test");
         card = new NumberCard(1, 5, null, UnoColor.RED);
 
         turn = new Turn(1, Turn.TurnType.PLAY_CARD, player);
@@ -93,7 +104,6 @@ public class GameLogicTest {
 
     @Test
     public void testCheckTurnChooseColor() {
-        player = new Player(1, "Test");
         player.setHasToChooseColor(true);
 
         turn = new Turn(1, Turn.TurnType.CHOOSE_COLOR, player);
@@ -103,15 +113,126 @@ public class GameLogicTest {
 
     @Test
     public void testCheckTurnCallUno() {
-        ArrayList<Card> hand = new ArrayList();
         hand.add(new NumberCard(1, 3, null, UnoColor.GREEN));
-
-        player = new Player(1, "Test");
         player.setHand(hand);
 
         turn = new Turn(1, Turn.TurnType.CALL_UNO, player);
 
         assertTrue(gameLogic.checkTurn(turn));
     }
-}
 
+    @Test
+    public void testDoTurnDraw() {
+        turn = new Turn(1, Turn.TurnType.DRAW, player);
+
+        gameSession.addPlayer(player);
+        gameState.setDrawCounter(0);
+
+        stack.add(new NumberCard(1, 3, null, UnoColor.GREEN));
+        gameState.setStack(stack);
+        gameLogic.doTurn(turn);
+
+        assertEquals(player.getHandCount(), 1);
+    }
+
+    @Test
+    public void testDoTurnChooseColor() {
+        turn = new Turn(1, Turn.TurnType.CHOOSE_COLOR, player);
+        turn.setColor(UnoColor.RED);
+
+        gameSession.addPlayer(player);
+        gameLogic.doTurn(turn);
+
+        assertEquals(gameState.getActualColor(), UnoColor.RED);
+        assertFalse(turn.getPlayer().hasToChooseColor());
+    }
+
+    @Test
+    public void testDoTurnCallUno() {
+        turn = new Turn(1, Turn.TurnType.CALL_UNO, player);
+
+        gameLogic.doTurn(turn);
+
+        assertEquals(turn.getPlayer().isUno(), true);
+    }
+
+    @Test
+    public void testDoTurnPlayNumberCard() {
+        Card card = new NumberCard(1, 5, null, UnoColor.RED);
+
+        turn = new Turn(1, Turn.TurnType.PLAY_CARD, player);
+        turn.setCard(card);
+
+        gameSession.addPlayer(player);
+
+        gameLogic.doTurn(turn);
+
+        assertEquals(gameState.sizeOfPlayedStack(), 1);
+        assertEquals(gameState.getActualColor(), UnoColor.RED);
+    }
+
+    @Test
+    public void testDoTurnPlayActionCardTwoActions() {
+        ActionCard card = new ActionCard(1, null, UnoColor.RED);
+
+        card.addAction(new Action(actionType1));
+        card.addAction(new Action(actionType2));
+
+        turn = new Turn(1, Turn.TurnType.PLAY_CARD, player);
+        turn.setCard(card);
+
+        gameLogic.doTurn(turn);
+
+        assertEquals(gameState.sizeOfPlayedStack(), 1);
+        assertEquals(gameState.getDrawCounter(), 4);
+        assertTrue(turn.getPlayer().hasToChooseColor());
+    }
+
+    @Test
+    public void testDoTurnPlayActionCardAdd2() {
+        ActionCard card = new ActionCard(1, null, UnoColor.RED);
+
+        card.addAction(new Action(actionType1));
+
+        turn = new Turn(1, Turn.TurnType.PLAY_CARD, player);
+        turn.setCard(card);
+
+        gameSession.addPlayer(player);
+        gameLogic.doTurn(turn);
+
+        assertEquals(gameState.sizeOfPlayedStack(), 1);
+        assertEquals(gameState.getDrawCounter(), 2);
+        assertEquals(gameState.getActualColor(), UnoColor.RED);
+    }
+
+    /*
+    @Test
+    public void testCheckCardNumberCard() {
+        Card playedStackCard = new NumberCard(1, 5, null, UnoColor.RED);
+        Card card = new NumberCard(1, 3, null, UnoColor.RED);
+
+        playedStack.add(playedStackCard);
+
+        gameState.setPlayedStack(playedStack);
+        gameState.setActualColor(UnoColor.RED);
+
+        assertTrue(gameLogic.checkCard(card));
+    }
+
+    @Test
+    public void testCheckCardActionCard() {
+        ActionCard playedStackCard = new ActionCard(1, null, UnoColor.RED);
+
+        playedStackCard.addAction(new Action(actionType1));
+
+        Card card = new NumberCard(1, 3, null, UnoColor.RED);
+
+        playedStack.add(playedStackCard);
+
+        gameState.setPlayedStack(playedStack);
+        gameState.setActualColor(UnoColor.RED);
+
+        assertTrue(gameLogic.checkCard(card));
+    }
+    */
+}
